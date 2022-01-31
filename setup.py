6bcd57dc6b405a16d7e6bd68e430fb281feea11b
Dipendra Pratap <dipendrapratap155@gmail.com>
2022-01-31 02:24:19 -0800

    stage 01 created

--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
-from platform import python_version
-from public import install
 from setuptools import setup
 
-with open('README.md',"r",encoding='utf-8') as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name = 'src'
-    ,version='0.0.1'
-    ,author='Dipendra Pratap'
-    ,description='A small package for dvc ml pipeline demo'
-    ,long_description=long_description
-    ,long_description_content_type="text/markdown"
-    ,url='https://github.com/dipdregan/DVC_ML_BASICE'
-    ,author_email='dipendrapratap155@gmail.com'
-    ,packages=['src']
-    ,python_requires=">3.7"
-    ,install_requires=[
+    name="src",
+    version="0.0.1",
+    author="Dipendra Pratap",
+    description="A small package for dvc ml pipeline demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dipdregan/DVC_ML_BASICE",
+    author_email="dipendrapratap155@gmail.com",
+    packages=["src"],
+    python_requires=">=3.7",
+    install_requires=[
         'dvc',
         'pandas',
         'scikit-learn'
     ]
-
-
-)
+) 
---
+++ src/stage_01_load_save.py
@@ -0,0 +1,45 @@
+from ast import arg
+from venv import create
+from src.utils.all_utils import read_yaml,create_directory
+import argparse
+import pandas as pd
+import os
+ 
+def get_data(config_path):
+    config = read_yaml(config_path)
+
+    #print(config)
+    """
+    op-
+    {'data_source': 'http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv', 'artifacts': {'artifacts_dir': 'artifacts', 'raw_local_dir': 'raw_local_dir', 'raw_local_file': 'data.csv'}}
+    """
+    remote_data_path = config['data_source']
+    df = pd.read_csv(remote_data_path,sep=";")
+    #print(df.head())
+
+    #save data set in the local directory
+    #create the path to directory: artifact dir/raw_local_dir/data.csv
+    artifact_dir = config["artifacts"]["artifacts_dir"]
+    raw_local_dir = config["artifacts"]["raw_local_dir"]
+    raw_local_file = config["artifacts"]["raw_local_file"]
+
+    raw_local_dir_path =os.path.join(artifact_dir,raw_local_dir)
+    create_directory(dirs=[raw_local_dir_path])
+    raw_local_file_path =os.path.join(raw_local_dir_path,raw_local_file)
+    
+    df.to_csv(raw_local_file_path,sep=',',index=False)
+     
+
+
+
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+
+    args.add_argument('--config',"-c",default="config/config.yaml")
+
+    parsed_args = args.parse_args()
+
+    #print(parsed_args)#op-Namespace(config='config/config.yaml')
+    #print(parsed_args.config) #op-config/config.yaml
+
+    get_data(config_path=parsed_args.config)
